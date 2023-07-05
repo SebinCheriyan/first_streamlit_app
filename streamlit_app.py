20d5af5539e8ab7d5cd27f76afc088ac31e4b862
SebinCheriyan <66517303+SebinCheriyan@users.noreply.github.com>
2023-07-05 16:51:50 +0100

    Update streamlit_app.py
    code to add user entry

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,11 @@ streamlit.dataframe(fruit_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json()) # Just writes the data on the screen
 
 # normalize the json version of the data 
